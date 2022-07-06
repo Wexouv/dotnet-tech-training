ac0a4d40de3c70db172595576d47b0d1a55c0abe
Alex Wexov <wexouv@gmail.com>
2022-07-06 15:20:30 +0500

    Added mapster testing library.

---
+++ mapster_test/Class1.cs
@@ -0,0 +1,19 @@
+﻿using Mapster;
+
+public class MapsterTesting
+{
+
+}
+
+public class User {
+    public string Name { get; set; }
+    public int Age { get; set; }
+    public string Email { get; set; }
+    public Address Address { get; set; }
+}
+
+public class Address {
+    public string Street { get; set; }
+    public string City { get; set; }
+    public string Country { get; set; }
+}
