693495c6a7f10b7e75d02ed68bf33361fd450f0a
Alex Wexov <wexouv@gmail.com>
2022-07-06 15:51:48 +0500

    Added some tests for mapping

--- mapster_test/Class1.cs
+++ mapster_test/Class1.cs
@@ -1,8 +1,35 @@
-﻿using Mapster;
+﻿using Newtonsoft.Json;
+using Mapster;
 
-public class MapsterTesting
+// Serialize object to json and print to console
+public class Program
 {
+    public static void Print(object obj)
+    {
+        Console.WriteLine(JsonConvert.SerializeObject(obj, Formatting.Indented));
+    }
 
+    public static void Main()
+    {
+        var user = new User
+        {
+            Name = "John",
+            Age = 30,
+            Email = "test@gmail.com",
+            Address = new Address
+            {
+                Street = "Main street",
+                City = "New York",
+                Country = "USA"
+            }
+        };
+
+        Print(user);
+        var smallDto = user.Adapt<SmallUserDto>();
+        Print(smallDto);
+        var fullDto = smallDto.Adapt<User>();
+        Print(fullDto);
+    }
 }
 
 public class User {
@@ -16,4 +43,10 @@ public class Address {
     public string Street { get; set; }
     public string City { get; set; }
     public string Country { get; set; }
+}
+
+public class SmallUserDto
+{
+    public string Name { get; set; }
+    public int Age { get; set; }
 }
